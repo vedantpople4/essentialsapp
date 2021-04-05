9e3a41c2f63cbf01c3d37381e64baccaa527b348
Vedant pople <vedantpople4@gmail.com>
2021-04-05 21:57:14 +0530

    Initial Commit

--- essentialsapp/lib/main.dart
+++ essentialsapp/lib/main.dart
@@ -28,7 +28,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 4;
+  int _counter = 0;
 
   void _incrementCounter() {
     setState(() {
